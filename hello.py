2918017b3d1a0b2c968b7941ca4650b2ebdca94c
AEISWARJA <aeiswarjabiswas98@gmail.com>
2021-05-19 01:52:52 +0530

    initialization of basic python

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Author : Aeiswarja
+# date : 19.05.2021
+from playsound import playsound
+
+playsound('F:\\pythonProgram\\ringtone.mp3')
+# print("Hello world")
+
+# print(''' Twinkle, twinkle, little star
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky
+# Twinkle, twinkle little star
+# How I wonder what you are ''')
